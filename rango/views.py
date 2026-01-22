6a7e60cbd39fb4c5501d1bb8de087158c96c2901
“Finn <2478426m@student.gla.ac.uk>
2026-01-22 17:55:21 +0000

    finished chapter 3

---
+++ rango/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Rango says hey there partner! <p><a href=\"/rango/about\">about this site</a></p>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.<p><a href=\"/rango\">back to main</a></p>")
